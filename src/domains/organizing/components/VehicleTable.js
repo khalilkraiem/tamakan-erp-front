b9d92e22f316a9ef7ad15c2c94643d7cbbe23e6b
khalil kraiem <khalil.kraiem@vegagroup.com.tn>
2022-10-21 16:34:18 +0100

    vehicle table

--- src/domains/organizing/components/VehicleTable.js
+++ src/domains/organizing/components/VehicleTable.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import data from '../../../constants/VehiclesData'
+import data from '../../../constants/vehiclesData'
 import eye from '../../../assets/images/eye.svg'
 import trash from '../../../assets/images/delete.svg'
 import edit from '../../../assets/images/edit.svg'
@@ -7,8 +7,8 @@ import up from '../../../assets/images/up.svg'
 import down from '../../../assets/images/down.svg'
 import LightButton from '../../../components/buttons/LightButton'
 import { useNavigate } from 'react-router-dom'
-function VehicleTable({setDeleteVehicleIsOpen}) {
-    let navigate=useNavigate()
+function VehicleTable({setDeleteVehicleIsOpen,setViewVehiculeOpen}) {
+    
     return (
         <div className='bgLightGray pad30 '>
             <div className='fzm'>
@@ -25,10 +25,10 @@ function VehicleTable({setDeleteVehicleIsOpen}) {
                                 <img alt='down' src={down} className='h10 w10 ' />
                             </div>
                         </div>
-                        <div className='w200 Center'><p>Name</p></div>
-                        <div className='w200 Center'><p>Country/City</p></div>
-                        <div className='w200 Center'><p>Phone</p></div>
-                        <div className='w300 Center'><p>Email</p></div>
+                        <div className='w200 Center'><p>Driver</p></div>
+                        <div className='w200 Center'><p>Car Plate</p></div>
+                        <div className='w200 Center'><p>IP</p></div>
+                       
                     </div>
                     <div className='cCenter w100 mar20'>
                         <div className='mar10'><p>Action</p></div>
@@ -42,14 +42,14 @@ function VehicleTable({setDeleteVehicleIsOpen}) {
                     <div key={i} className={`spaceBetween h50 ${i % 2 === 1 ? "bgWhite" : "bgLightGray "} line`}>
                         <div className='row'>
                             <div className='w100 Center fzm mar20 '><p className='mar10'>{elem.srNo}</p></div>
-                            <div className='w200 Center fzm'><p>{elem.name}</p></div>
-                            <div className='w200 Center fzm'><p>{elem.country}/{elem.city}</p></div>
-                            <div className='w200 Center fzm'><p>{elem.phone}</p></div>
-                            <div className='w300 Center fzm'><p>{elem.email}</p></div>
+                            <div className='w200 Center fzm'><p>{elem.driver}</p></div>
+                            <div className='w200 Center fzm'><p>{elem.carPlate}</p></div>
+                            <div className='w200 Center fzm'><p>{elem.ip}</p></div>
+
                         </div>
                         <div className='w100 cCenter mar20'>
 
-                            <div className='h20 w20 br5 bgLightOrange cCenter mar5 pointer' onClick={()=>navigate("oneVehicle")}>
+                            <div className='h20 w20 br5 bgLightOrange cCenter mar5 pointer' onClick={()=>setViewVehiculeOpen(true)}>
                                 <img alt='eye' src={eye} className='' />
                             </div>
 
